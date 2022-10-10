1feb06c06c93718a8acace039d70724bf7d3e6d7
ArminasStrolys <59365832+ArminasStrolys@users.noreply.github.com>
2022-10-10 18:55:41 +0300

    Practicing flexbox starter functions

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,5 +1,15 @@
 function App() {
-  return <div>TESTING</div>;
+  return (
+    <>
+      <div className="container">
+        <item className="item">1</item>
+        <item className="item">2</item>
+        <item className="item long">3</item>
+        <item className="item">4</item>
+        <item className="item">5</item>
+      </div>
+    </>
+  );
 }
 
 export default App;
