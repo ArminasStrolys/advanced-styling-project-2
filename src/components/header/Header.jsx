3d289c89ef91de64fc525ecbad5fd71589bd05ce
ArminasStrolys <59365832+ArminasStrolys@users.noreply.github.com>
2022-10-12 17:59:50 +0300

    Structuring search bar with svgs

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 import search from '../../images/svg/search.svg';
 import banner from '../../images/svg/banner.svg';
 import message from '../../images/svg/message.svg';
+import logo from '../../images/svg/logo.svg';
+import user from '../../images/user.jpg';
 
 const Header = () => {
   return (
     <>
       <header className="header">
-        <img src="" alt="logo of the site" className="logo" />
+        <img src={logo} alt="logo of the site" className="logo" />
         <form action="#" className="search">
           <input
             type="text"
@@ -20,13 +22,29 @@ const Header = () => {
         </form>
         <nav className="user-nav">
           <div className="user-nav__icon-box">
-            <img src={banner} alt="" className="user-nav__icon" />
+            <img
+              src={banner}
+              alt=""
+              className="user-nav__icon user-nav__icon--banner"
+            />
           </div>
           <span className="user-nav__notification">5</span>
           <div className="user-nav__icon-box">
-            <img src={message} alt="" className="user-nav__icon" />
+            <img
+              src={message}
+              alt=""
+              className="user-nav__icon user-nav__icon--message"
+            />
+            <span className="user-nav__notification">16</span>
           </div>
-          <span className="user-nav__notification">16</span>
+          <div className="user-nav__user">
+            <img
+              src={user}
+              alt="small user avatar"
+              className="user-nav__user-photo"
+            />
+            <span className="user-nav__user-name">Loreta</span>
+          </div>
         </nav>
       </header>
     </>
