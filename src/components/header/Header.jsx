a313a14c6376bd34fc99adc6d0659cef8cd1e179
ArminasStrolys <59365832+ArminasStrolys@users.noreply.github.com>
2022-10-12 17:05:34 +0300

    Changed readme for the future.

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -3,7 +3,17 @@ import React from 'react';
 const Header = () => {
   return (
     <>
-      <header className="header">THIS IS A HEADER ELEMENT</header>
+      <header className="header">
+        <img src="" alt="logo of the site" className="logo" />
+        <form action="#" className="search">
+          <input
+            type="text"
+            className="search__input"
+            placeholder="Search hotels"
+          />
+          <button className="search__button"></button>
+        </form>
+      </header>
     </>
   );
 };
